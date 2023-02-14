59d0f53df21d5ac8b7b3c1e35325674336144265
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-02-14 10:25:51 -0300

    criando card

--- app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -3,20 +3,21 @@ package br.senai.sp.jandira.bmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,6 +26,7 @@ import br.senai.sp.jandira.bmicalculator.ui.theme.BMICalculatorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             BMICalculatorTheme {
                 CalculatorScreen()
@@ -36,6 +38,11 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun CalculatorScreen() {
+
+    var weightState = remember {
+        mutableStateOf(0)
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize()) {
             Column(
@@ -76,7 +83,7 @@ fun CalculatorScreen() {
                     modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
                 )
                 OutlinedTextField(
-                    value = "",
+                    value = "Senai",
                     onValueChange = {},
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(16.dp)
@@ -93,8 +100,54 @@ fun CalculatorScreen() {
                     )
                 }
             }
+            //footer
             Column() {
+                Card(
+                    modifier = Modifier.fillMaxSize(),
+                    shape = RoundedCornerShape(
+                        topStart = 48.dp,
+                        topEnd = 48.dp
+                    ),
+                    backgroundColor = Color(
+                        red = 79,
+                        green = 54,
+                        blue = 232
+                    )
 
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.your_score),
+                            color = Color.White,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            text = "0.0",
+                            color = Color.White,
+                            fontSize = 48.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = stringResource(id = R.string.ideal_state),
+                            color = Color.White,
+                            fontSize = 20.sp
+                        )
+                        Row() {
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = stringResource(id = R.string.reset))
+                            }
+                            Spacer(modifier = Modifier.width(24.dp))
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = stringResource(id = R.string.share))
+                            }
+                        }
+                    }
+                }
             }
         }
     }
